c127b775e8fcf1250444a8075ef046ce95974ef1
thannie-phan <thannie-phan@gmail.com>
2025-10-14 14:15:09 +1100

    add comment to facade

--- v1/app/services/facade.py
+++ v1/app/services/facade.py
@@ -5,7 +5,7 @@ from app.models.review import Review
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
+        self.user_repo = InMemoryRepository() # connect to memory for testing before connecting to actual database later
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
