c2c43257940cca376713a53475a07177ef0bb81b
thannie-phan <thannie-phan@gmail.com>
2025-10-27 09:54:54 +1100

    change everything app/__init to v2

--- v2/app/__init__.py
+++ v2/app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
-from app.api.v1.users import api as users_ns
-from app.api.v1.reviews import api as reviews_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.amenities import api as amenities_ns
+from app.api.v2.users import api as users_ns
+from app.api.v2.reviews import api as reviews_ns
+from app.api.v2.places import api as places_ns
+from app.api.v2.amenities import api as amenities_ns
 
 
 db = SQLAlchemy()
@@ -12,14 +12,14 @@ db = SQLAlchemy()
 
 def create_app(): # create multiple indendepent instances of app for test/stage
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+    api = Api(app, version='2.0', title='HBnB API', description='HBnB Application API', doc='/api/v2/')
 
     # register the users namespace, allowing the route defined in api/v1/users to be accessible through /api/v1/users
-    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(users_ns, path='/api/v2/users')
     # register the places namespace, allowing the route defined in api/v1/places to be accessible through /api/v1/places
-    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(places_ns, path='/api/v2/places')
     # register the amenities namespace, allowing the route defined in api/v1/amenities to be accessible through /api/v1/amenities
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(amenities_ns, path='/api/v2/amenities')
     # register reviews namespace, allowing the route defined in api/v1/reviews to be accessible through /api/v1/reviews
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(reviews_ns, path='/api/v2/reviews')
     return app
