940a324889fd38f449d646deb81b6bc584789b52
thannie-phan <thannie-phan@gmail.com>
2025-11-05 11:30:38 +1100

    change usermodel inside user api

--- v1/app/api/v1/users.py
+++ v1/app/api/v1/users.py
@@ -11,7 +11,8 @@ user_model = api.model('User', {
     'first_name': fields.String(required=True, description='First name of the user'),
     'last_name': fields.String(required=True, description='Last name of the user'),
     'email': fields.String(required=True, description='Email of the user'),
-    'password': fields.String(required=True, description='Password of the user')
+    'password': fields.String(required=True, description='Password of the user'),
+    'is_admin': fields.Boolean(required=False, description='If user is admin')
 })
 
 # CREATE USER
