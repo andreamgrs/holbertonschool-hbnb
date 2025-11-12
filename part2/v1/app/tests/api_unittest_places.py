0259fee8cc77fdffd62f063514092be35d834865
sheeny84 <roisin.soulsby@gmail.com>
2025-11-12 11:41:23 +1100

    Update folder structure

---
+++ part2/v1/app/tests/api_unittest_places.py
@@ -0,0 +1,131 @@
+import unittest
+from app import create_app
+from app.models.user import User
+from app.models.place import Place
+from app.models.review import Review
+import json
+###
+# --- API Endpoint Tests ---
+class TestPlaceEndpoints(unittest.TestCase):
+    test_owner_id = ''
+    test_place_id = ''
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+    
+    def test_01_get_all_places_empty(self):
+        response_places = self.client.get('/api/v1/places/')
+        self.assertEqual(json.loads(response_places.data), [])
+        self.assertEqual(response_places.status_code, 200)
+
+    def test_02_create_place(self):
+        global test_owner_id
+        global test_place_id
+        # Create user first
+        response_user = self.client.post('/api/v1/users/', json={
+            "first_name": "Jane",
+            "last_name": "Doe",
+            "email": "jane.doe@example.com"
+        })
+        self.assertEqual(response_user.status_code, 201)
+        # Get user id to use as owner id
+        user_data = json.loads(response_user.data)
+        test_owner_id = user_data["id"]
+        # Create place
+        request_place_data = {
+            "title": "Cozy Apartment",
+            "description": "A nice place to stay",
+            "price": 100.0,
+            "latitude": 37.7749,
+            "longitude": -122.4194,
+            "owner_id": test_owner_id,
+            "amenities": []
+            }
+        response_places = self.client.post('/api/v1/places/', json=request_place_data)
+        response_place_data = json.loads(response_places.data)
+        test_place_id = response_place_data["id"]
+        # Remove id from response for comparison with request
+        del response_place_data["id"]
+        del request_place_data["amenities"]
+        self.assertEqual(request_place_data, response_place_data)
+        self.assertEqual(response_places.status_code, 201)
+
+    def test_03_create_place_invalid_data(self):
+        response_places = self.client.post('/api/v1/places/', json={
+            "title": "",
+            "description": "",
+            "price": -10,
+            "latitude": 100.0,
+            "longitude": -200.0,
+            "owner_id": test_owner_id
+            })
+        self.assertEqual(response_places.status_code, 400)
+
+    def test_04_get_place_list_one_place(self):
+        # Request data is from test_02
+        request_place_data = {
+            "title": "Cozy Apartment",
+            "price": 100.0,
+            "latitude": 37.7749,
+            "longitude": -122.4194
+            }
+        response = self.client.get('/api/v1/places/')
+        response_data = json.loads(response.data)
+        # Get id of first place in list
+        response_place = response_data[0]
+        self.assertEqual(response_place['id'], test_place_id)
+        # Remove id from response for comparison with request
+        del response_place['id']
+        self.assertEqual(request_place_data, response_place)
+
+    def test_05_get_place_data(self):
+        endpoint = '/api/v1/places/' + str(test_place_id)
+        response = self.client.get(endpoint)
+        response_data = json.loads(response.data)
+        expected_response = {
+            "id": test_place_id,
+            "title": "Cozy Apartment",
+            "description": "A nice place to stay",
+            "price": 100.0,
+            "latitude": 37.7749,
+            "longitude": -122.4194,
+            "owner": {
+                "id": test_owner_id,
+                "first_name": "Jane",
+                "last_name": "Doe",
+                "email": "jane.doe@example.com"
+            },
+            "amenities": []
+        }
+        self.assertEqual(response_data, expected_response)
+    
+    # Update place data
+    def test_06_update_place_invalid_input(self):
+        endpoint = '/api/v1/places/' + str(test_place_id)
+        response = self.client.put(endpoint, json='')
+        self.assertEqual(response.status_code, 400)
+
+    def test_07_update_place_not_found(self):
+        # provide random place id
+        endpoint = '/api/v1/places/' + '7c1acc68-76a9-4a3c-8949-9c4c36469817'
+        response = self.client.put(endpoint, json={
+            "title": "Luxury Condo",
+            "description": "An upscale location",
+            "price": 200.0
+        })
+        response_data = json.loads(response.data)
+        self.assertEqual(response.status_code, 404)
+
+    def test_08_update_place(self):
+        endpoint = '/api/v1/places/' + str(test_place_id)
+        response = self.client.put(endpoint, json={
+            "title": "Luxury Condo",
+            "description": "An upscale location",
+            "price": 200.0
+        })
+        response_data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ part2/v1/app/tests/api_unittest_review.py
@@ -0,0 +1,130 @@
+"""
+1. start your server with python3 run.py
+2. Create an user POST http://localhost:5000/api/v1/users/ 
+3. Create a place POST http://localhost:5000/api/v1/places/ 
+4. Create a review POST http://localhost:5000/api/v1/reviews/
+5. GET review GET http://localhost:5000/api/v1/reviews/
+6. Update review PUT http://localhost:5000api/v1/reviews/<review_id>
+7. Get review again to see update GET http://localhost:5000/api/v1/reviews/
+
+"""
+
+import json
+import unittest
+from app import create_app
+from app.models.user import User
+from app.models.place import Place
+from app.models.review import Review
+
+# testing models 
+class TestReviewEndpoints(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+
+    def test_create_review(self):
+        #First create user 
+        user_response = self.client.post('/api/v1/users/', json={
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "john.doe@example.com"
+        })
+        #raw content of the HTTP response. If the server returns JSON, response.data will contain that JSON as a byte string.
+        user_data = json.loads(user_response.data) 
+        owner_id = user_data['id']
+        self.assertEqual(user_response.status_code, 201) #It verify that user_response.status_code is equal to 201, 
+        #user_data = user_response.get_json()
+        #user_id = user_data.get("id")
+        #self.assertIsNotNone(user_id) #check that user_id is not None
+        print("ID of the owner: {}".format(owner_id))
+
+
+        # Then, create a place
+        place_response = self.client.post('/api/v1/places/', json={
+            "title": "Cozy Apartment",
+            "description": "A nice place to stay",
+            "price": 100.0,
+            "latitude": 37.7749,
+            "longitude": -122.4194,
+            "owner_id": owner_id,
+            "amenities": []
+        })
+        self.assertEqual(place_response.status_code, 201)
+        #Get the id from places 
+        place_data = json.loads(place_response.data)
+        place_id = place_data['id']
+        print("ID of the place: {}".format(place_id))
+
+
+        # Create a review
+        review_response = self.client.post('/api/v1/reviews/', json={
+            "text": "Great place to stay!",
+            "rating": 5,
+            "user_id": owner_id,
+            "place_id": place_id
+        })
+        self.assertEqual(review_response.status_code, 201)
+         #Get the id from the review
+        review_data = json.loads(review_response.data)
+        review_id = review_data['id']
+        print("ID of review: {}".format(review_id))
+
+        # Update a review
+        update_review_response = self.client.put(f'/api/v1/reviews/{review_id}', json={
+            "text": "Not bad",
+            "rating": 5
+        })
+        self.assertEqual(update_review_response.status_code, 200)
+
+        # Verify that data was updated
+        updated_data = json.loads(update_review_response.data)
+        self.assertEqual(updated_data['message'], "Review updated successfully")
+
+
+        #Check the text is not empty 
+        review_empty_text_response = self.client.post('/api/v1/reviews/', json={
+            "text": "",
+            "rating": 5,
+            "user_id": owner_id,
+            "place_id": place_id
+        })
+        self.assertEqual(review_empty_text_response.status_code, 400) #400 if input data invalid
+        error_text_data = json.loads(review_empty_text_response.data)
+        print("Status Code:", review_empty_text_response.status_code,"Error:", error_text_data.get("message")) 
+
+        #Check the owner_id does not exist
+        review_bad_owner_id_response = self.client.post('/api/v1/reviews/', json={
+            "text": "Good space",
+            "rating": 5,
+            "user_id": "jbcjdcj-jbc",
+            "place_id": place_id
+        })
+        self.assertEqual(review_bad_owner_id_response.status_code, 400) #404 if does not exist
+        error_owner_data = json.loads(review_bad_owner_id_response.data)
+        print("Status Code:", review_bad_owner_id_response.status_code, "Error:", error_owner_data.get("message"))    
+
+        #Check the place_id does not exist
+        review_bad_place_id_response = self.client.post('/api/v1/reviews/', json={
+            "text": "Good space",
+            "rating": 5,
+            "user_id": owner_id,
+            "place_id": "jbcjdcj-jbc"
+        })
+        self.assertEqual(review_bad_place_id_response.status_code, 404) #404 if does not exist
+        error_place_data = json.loads(review_bad_place_id_response.data)
+        print("Status Code:", review_bad_place_id_response.status_code, "Error:", error_place_data.get("message"))
+
+        #Check the place_id is empty string
+        review_empty_place_response = self.client.post('/api/v1/reviews/', json={
+            "text": "Good space",
+            "rating": 5,
+            "user_id": owner_id,
+            "place_id": ""
+        })
+        self.assertEqual(review_empty_place_response.status_code, 400) #400 if input data invalid
+        error_empty_place_data = json.loads(review_empty_place_response.data)
+        print("Status Code:", review_empty_place_response.status_code, "Error:", error_empty_place_data.get("message"))
+
+
+
