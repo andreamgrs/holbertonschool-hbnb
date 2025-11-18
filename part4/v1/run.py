f23591b945c78f3b4009c74996ea83c5e34a3fbd
andreamgrs <andrea.munozgr@gmail.com>
2025-11-18 14:33:13 +1100

    Adding task 1

--- part4/v1/run.py
+++ part4/v1/run.py
@@ -1,6 +1,14 @@
 from app import create_app
+from flask import Flask, render_template
 
 app = create_app()
 
+
+@app.route('/api/v1/auth/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
+
+
