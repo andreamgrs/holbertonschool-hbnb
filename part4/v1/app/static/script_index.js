219ad8df8305a1bf17549a3cf51a43db991e0ec9
sheeny84 <roisin.soulsby@gmail.com>
2025-11-27 14:53:39 +1100

    Clean up code for index

--- part4/v1/app/static/script_index.js
+++ part4/v1/app/static/script_index.js
@@ -1,48 +1,46 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
-
-
+// Check authentication and display price filter once page is loaded
 document.addEventListener('DOMContentLoaded', () => {
   checkAuthentication();
   const price_filter = document.getElementById('price-filter');
   price_filter.innerHTML = '<option value="10">10</option><option value="50">50</option><option value="100">100</option><option value="All">All</option>'
 });
 
-
+// Check user is authenticated
 function checkAuthentication() {
   const token = getCookie('token');
   const loginLink = document.getElementById('login-button');
 
+  // Hide login link if user is already logged in
   if (!token) {
     loginLink.style.display = 'block';
   } else {
     loginLink.style.display = 'none';
-
-    // Fetch places data if the user is authenticated
   }
+
+  // Fetch places regardless of whether user is logged in
   fetchPlaces(token);
 }
 
+// Function to get a cookie value by its name
 function getCookie(name) {
-  // Function to get a cookie value by its name
-  console.log(name);
+  // Get cookie string and split into list of cookies
   cookies = document.cookie.split(";")
   let cookie_name = name + "=";
-  console.log(cookies);
+  // Iterate through cookies
   for (let i = 0; i < cookies.length; i++) {
     let c = cookies[i];
+    // Remove leading spaces
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
     }
+    // Check if cookie starts with cookie_name
     if (c.indexOf(cookie_name) == 0) {
-      console.log("the return", c.substring(cookie_name.length, c.length))
+      // Return everything after the cookie name 
+      // i.e. value of the cookie
       return c.substring(cookie_name.length, c.length);
     }
   }
 }
-
 
 async function fetchPlaces(token) {
   // Make a GET request to fetch places data
@@ -52,10 +50,8 @@ async function fetchPlaces(token) {
       'Authorization': `Bearer ${token}`
     }
   });
-  // Include the token in the Authorization header
   // Handle the response and pass the data to displayPlaces function
   if (response.ok) {
-    //alert('Response Places OK: ' + response.statusText);
     const data = await response.json();
     displayPlaces(data)
 
@@ -65,53 +61,45 @@ async function fetchPlaces(token) {
 }
 
 function displayPlaces(places) {
-  // Clear the current content of the places list
-  // Iterate over the places data
-  // For each place, create a div element and set its content
-  // Append the created element to the places list
   const place_list = document.getElementById('places-list');
+  // Clear the content of the places list
   place_list.innerHTML = " "
+  // Iterate over the places
   places.forEach(place => {
-    console.log(places);
+    // Create a div for each place and add the place info
+    // Use place_html string to prevent closing elements being added
     let place_html = '<div class="place-card">';
     place_html += "<p>" + place.title + "</p>";
     place_html += `<img src="/static/images/${place.id}.jpg" class="place-image">`;
     place_html += '<p class="place-price">' + "$" + place.price + "</p>";
     place_html += '<button class="details-button"><a href="place?id=' + place.id + '" id="details-button">View Details</a></button>'
     place_html += '</div>'
+    // Add place html to the inner html
     place_list.innerHTML += place_html;
-    console.log(place_list.innerHTML);
   });
 }
 
 document.getElementById('price-filter').addEventListener('change', (event) => {
-  // Get the selected price value
   const place_list = document.getElementById('places-list');
   const price_list = document.querySelectorAll('.place-price');
-  console.log(price_list)
+  // Get the selected max price from the price filter
   const max_price = event.target.value;
-  console.log(max_price);
-  let counter = 0
+  
+  // Iterate through the place cards
+  let counter = 0 // Counter for price list
   for (const place_card of place_list.children) {
-    let price_of_place = price_list[counter].textContent;
-    console.log(price_of_place)
-    price_of_place = price_of_place.substring(1);
-    console.log(price_of_place);
-    if (max_price != "All") {
-      place_card.style.display = "none";
-    } else {
+    // Get price from place card
+    let place_price = price_list[counter].textContent;
+    // Remove leading '$'
+    place_price = place_price.substring(1);
+    // Display place card if price filter is All or place price is less than max price
+    if ((max_price == "All") || (parseInt(place_price) <= parseInt(max_price))) {
       place_card.style.display = "block";
-    }
-    if (parseInt(price_of_place) > parseInt(max_price)) {
-      place_card.style.display = "none";
     } else {
-      place_card.style.display = "block";
+      place_card.style.display = "none";
     }
-
     counter += 1;
   }
-
-  // Iterate over the places and show/hide them based on the selected price
 });
 
 
